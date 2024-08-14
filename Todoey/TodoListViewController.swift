2e8260ee025bd736055202239bcd781e710fe107
Anket Kohak <anketkohak40@gmail.com>
2024-08-14 11:08:35 +0530

    basic ui is ready of table view 234

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+let itemArray = ["Find Mike","Buy eggs","Destroy Demogarban"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 
 }
 
