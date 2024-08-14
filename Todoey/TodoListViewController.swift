c413314366c5e2b7fe4c61e8a59d058699c9e96c
Anket Kohak <anketkohak40@gmail.com>
2024-08-14 10:20:44 +0530

    Initial app Setup

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
