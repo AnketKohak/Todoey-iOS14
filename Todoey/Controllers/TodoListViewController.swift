b253b7a68ef5e828cdf9ab1cbd69d0cee97d66c0
Anket Kohak <anketkohak40@gmail.com>
2024-08-15 20:00:58 +0530

    searchBar add functionality Go Back to the Original List 257

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -91,4 +91,14 @@ extension TodoListViewController : UISearchBarDelegate{
         
         loadItem(with: request)
     }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItem()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }else{
+            
+        }
+    }
 }
